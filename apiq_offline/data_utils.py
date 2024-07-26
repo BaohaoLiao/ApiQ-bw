f90b10a25c4f5033e2704e27d5f9159e217de695
baohaoliao <liaobaohao@gmail.com>
2024-07-26 14:35:37 +0200

    debug

--- apiq_offline/data_utils.py
+++ apiq_offline/data_utils.py
@@ -90,7 +90,7 @@ def get_c4(tokenizer, nsamples, seed, seqlen, cache_dir):
         while True:
             i = random.randint(0, len(valdata) - 1)
             tmp = tokenizer(valdata[i]['text'], return_tensors='pt')
-            if tmp.input_ids.shape[1] >= seqlen:
+            if tmp.input_ids.shape[1] > seqlen:
                 break
         i = random.randint(0, tmp.input_ids.shape[1] - seqlen - 1)
         j = i + seqlen
